ee3b65a520aef93592e2b5c45c74fe969bd30ed1
bluespada <pentingmain@gmail.com>
2021-10-16 17:32:00 +0800

    feat: change vimwiki extension from .wiki to .md

--- lua/plugins-settings.lua
+++ lua/plugins-settings.lua
@@ -115,8 +115,8 @@ require'zen-mode'.setup{}
 
 -- wiki
 vim.g.vimwiki_list = {{
-    path = '~/MyNotes', -- this is a path of your notes / wiki directory
-    ext = '.wiki' -- i use wiki extension but if u want you can change it to .md files
+    path = '~/.Note', -- this is a path of your notes / wiki directory
+    ext = '.md' -- i use wiki extension but if u want you can change it to .md files
     -- for more informations you can see : https://github.com/vimwiki/vimwiki
 }}
 
