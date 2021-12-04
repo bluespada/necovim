7474522bf0b28265cebda96cb41ddcd06441d34b
bluespada <pentingmain@gmail.com>
2021-12-04 08:40:02 +0800

    fix : fix lsp

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -11,19 +11,19 @@ local lsp_signature_cfg = {
     hint_prefix = "😈 ",
 }
 
+
 local on_attach = function(client,bufnr)
     -- enable completion
-    
     if(lsp_signature_cfg~=nil)then
         require'lsp_signature'.on_attach(lsp_signature_cfg)
     end
     -- require'completion'.on_attach(client,bufnr) -- disable completion
-    if client.resolved_capabilities.document_formatting then
-        vim.api.nvim_command [[augroup Format]]
-        vim.api.nvim_command [[autocmd! * <buffer>]]
-        vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-        vim.api.nvim_command [[augroup END]]
-    end
+    --[[if client.resolved_capabilities.document_formatting then]]
+        --[[vim.api.nvim_command [[augroup Format]]
+        --[[vim.api.nvim_command [[autocmd! * <buffer>]]
+        --[[vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+        --[[vim.api.nvim_command [[augroup END]]
+    --[[end]]
 end
 
 -- lspkind
@@ -58,9 +58,9 @@ cmp.setup {
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'vsnip' },
-		{ name = 'look'}
 		}, {
 		{ name = 'buffer' },
+        { name = 'look' }
 	})
 }
 
@@ -71,13 +71,12 @@ nvim_lsp.vimls.setup {
 }
 
 -- settings flutter-tools
-require'flutter-tools'.setup {
-    flutter_path = "/opt/flutter/bin/flutter",
-    lsp = {
-        on_attach = on_attach,
-        capabilities = capabilities
-    }
-}
+--[[require'flutter-tools'.setup {]]
+    --[[flutter_path = "/opt/flutter/bin/flutter",]]
+    --[[lsp = {]]
+        --[[capabilities = lsp_cmp.update_capabilities(capabilities),]]
+    --[[}]]
+--[[}]]
 
 require'config.codeaction'
 
@@ -100,62 +99,62 @@ end)
 
 -- auto format
 
-nvim_lsp.diagnosticls.setup {
-  on_attach = on_attach,
-  filetypes = { 'python', 'xml', 'lua', 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown', 'pandoc' },
-  init_options = {
-    linters = {
-      eslint = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
-        debounce = 100,
-        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-        sourceName = 'eslint_d',
-        parseJson = {
-          errorsRoot = '[0].messages',
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '[eslint] ${message} [${ruleId}]',
-          security = 'severity'
-        },
-        securities = {
-          [2] = 'error',
-          [1] = 'warning'
-        }
-      },
-    },
-    filetypes = {
-      javascript = 'eslint',
-      javascriptreact = 'eslint',
-      typescript = 'eslint',
-      typescriptreact = 'eslint',
-    },
-    formatters = {
-      eslint_d = {
-        command = 'eslint_d',
-        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
-        rootPatterns = { '.git' },
-      },
-      prettier = {
-        command = 'prettier',
-        args = { '--stdin-filepath', '%filename' }
-      }
-    },
-    formatFiletypes = {
-        css = 'prettier',
-        javascript = 'eslint_d',
-        javascriptreact = 'eslint_d',
-        json = 'prettier',
-        scss = 'prettier',
-        less = 'prettier',
-        typescript = 'eslint_d',
-        typescriptreact = 'eslint_d',
-        markdown = 'prettier',
-        python = 'prettier',
-        xml = "prettier",
-        lua = "prettier",
-    }
-  }
-}
+--[[nvim_lsp.diagnosticls.setup {]]
+  --[[on_attach = on_attach,]]
+  --[[filetypes = { 'python', 'xml', 'lua', 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown', 'pandoc' },]]
+  --[[init_options = {]]
+    --[[linters = {]]
+      --[[eslint = {]]
+        --[[command = 'eslint_d',]]
+        --[[rootPatterns = { '.git' },]]
+        --[[debounce = 100,]]
+        --[[args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },]]
+        --[[sourceName = 'eslint_d',]]
+        --[[parseJson = {]]
+          --[[errorsRoot = '[0].messages',]]
+          --[[line = 'line',]]
+          --[[column = 'column',]]
+          --[[endLine = 'endLine',]]
+          --[[endColumn = 'endColumn',]]
+          --[[message = '[eslint] ${message} [${ruleId}]',]]
+          --[[security = 'severity']]
+        --[[},]]
+        --[[securities = {]]
+          --[[[2] = 'error',]]
+          --[[[1] = 'warning']]
+        --[[}]]
+      --[[},]]
+    --[[},]]
+    --[[filetypes = {]]
+      --[[javascript = 'eslint',]]
+      --[[javascriptreact = 'eslint',]]
+      --[[typescript = 'eslint',]]
+      --[[typescriptreact = 'eslint',]]
+    --[[},]]
+    --[[formatters = {]]
+      --[[eslint_d = {]]
+        --[[command = 'eslint_d',]]
+        --[[args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },]]
+        --[[rootPatterns = { '.git' },]]
+      --[[},]]
+      --[[prettier = {]]
+        --[[command = 'prettier',]]
+        --[[args = { '--stdin-filepath', '%filename' }]]
+      --[[}]]
+    --[[},]]
+    --[[formatFiletypes = {]]
+        --[[css = 'prettier',]]
+        --[[javascript = 'eslint_d',]]
+        --[[javascriptreact = 'eslint_d',]]
+        --[[json = 'prettier',]]
+        --[[scss = 'prettier',]]
+        --[[less = 'prettier',]]
+        --[[typescript = 'eslint_d',]]
+        --[[typescriptreact = 'eslint_d',]]
+        --[[markdown = 'prettier',]]
+        --[[python = 'prettier',]]
+        --[[xml = "prettier",]]
+        --[[lua = "prettier",]]
+    --[[}]]
+  --[[}]]
+--[[}]]
