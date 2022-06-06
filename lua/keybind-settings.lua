5c5c2efc6ec1f26315f33bb2121f46bca959fdea
bluespada <pentingmain@gmail.com>
2022-06-06 08:42:32 +0800

    feat : add toggleterm keybind settings

--- lua/keybind-settings.lua
+++ lua/keybind-settings.lua
@@ -58,3 +58,5 @@ map('n', '<leader>tc', ':TroubleClose<CR>', { silent = true, noremap = true })
 -- Github
 map('n', '<leader>gdd', ':Git add .<CR>', { silent = true, noremap = true })
 map('n', '<leader>gdc', ':Git commit<CR>', { silent = true, noremap = true })
+-- ToggleTerm
+map('n','<leader>;',':ToggleTerm <CR>',{ silent = true, noremap = true })
