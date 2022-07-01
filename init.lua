8b9e00eabe7ea6cccee8ef02c0e73fa2724a4482
bluespada <pentingmain@gmail.com>
2022-07-01 14:03:45 +0800

    feat : final fix

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require 'plugins' -- list of all neovim plugins + packer configurations
 require 'nvim-settings' -- neovim configurations
--- require'keybind-settings' -- neovim keybind configurations
 require 'plugins-settings' -- plugins settings 
-require 'keymap'
+require 'keybind-settings' -- keybind
