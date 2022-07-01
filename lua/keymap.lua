585c8c1cb9c0f9eaec97aa9d85d96815b5cb1e66
bluespada <pentingmain@gmail.com>
2022-07-01 14:02:39 +0800

    feat : final key

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -17,27 +17,65 @@ map.register({
             d = { "<cmd>bd!<cr>", "Close Current buffers" },
             n = { "<cmd>bnext<cr>", "Next Buffers" },
             p = { "<cmd>bprevious<cr>", "Previous Buffers" },
-        }
-        -- NerdTree
+            m = {
+                name = "Move",
+                n = { "<cmd>BufferLineMoveNext<cr>", "Move buffers to next" },
+                p = { "<cmd>BufferLineMovePrev<cr>", "Move buffer to previus" }
+            }
+
+        },
+        c = { "Calendar" },
+        -- calendar
+        ['ca'] = {
+            name = "Calendar",
+            l = { "<cmd>Calendar", "Open Calendar" }
+        },
     },
     ['\\'] = {
-        ['nt'] = {
-            name = "NerdTree",
-            t = { "<cmd>NvimTreeToggle<cr>", "Toggle NerdTree" },
-            f = { "<cmd>NvimTreeFocus<cr>", "Focus NerdTree" },
-            c = { "<cmd>NvimTreeClose<cr>", "Close NerdTree" },
-            r = { "<cmd>NvimTreeRefresh<cr>", "Refresh NerdTree" },
+        -- NerdCommenter
+        ['c'] = "NerdCommenter",
+        -- NerdTree
+        n = {
+            name = "Nerd Tree",
+            t = {
+                name = "NerdTree",
+                t = { "<cmd>NvimTreeToggle<cr>", "Toggle NerdTree" },
+                f = { "<cmd>NvimTreeFocus<cr>", "Focus NerdTree" },
+                c = { "<cmd>NvimTreeClose<cr>", "Close NerdTree" },
+                r = { "<cmd>NvimTreeRefresh<cr>", "Refresh NerdTree" },
+            },
         },
+        -- toggle term
         [';'] = {
             "<cmd>ToggleTerm<cr>",
             "Open Terminal",
         },
+        -- github
+        g = { "Github" },
         ['gh'] = {
             name = "Github",
             d = { "<cmd>Git add .<cr>", "Github add All" },
             c = { "<cmd>Git commit<cr>", "Github Commit" },
+            p = { "Pull & Push" },
             ['pl'] = { "<cmd>Git pull<cr>", "Github Pull" },
-            ['ps'] = { "<cmd>Git push<cr>", "Github push" },
+            ['ph'] = { "<cmd>Git push<cr>", "Github push" },
+        },
+        -- trouble line
+        ['t'] = {
+            name = "TroubleLine,TagBar",
+            t = { "<cmd>TroubleToggle<cr>", "Toggle Trouble Line" },
+            r = { "<cmd>TroubleRefresh<cr>", "Refresh Trouble Line" },
+            c = { "<cmd>TroubleClose<cr>", "Close Trouble Line" },
+        },
+        -- tagbar
+        ['tb'] = {
+            name = "TagBar",
+            t = { "<cmd>TagbarToggle<cr>", "TagBar Toggle" },
+        },
+        -- VimWiki
+        w = {
+            name = "VimWiki",
+            ["\\"] = "VimWiki Extra",
         },
     }
 }, { mode = 'n', silent = true, noremap = true, nowait = true })
