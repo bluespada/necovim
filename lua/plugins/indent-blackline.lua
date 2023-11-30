317b04d8439af05a541f820e2fe407fba9f09592
bluespada <pentingmain@gmail.com>
2023-12-01 01:20:31 +0800

    fix: indent blank line

--- lua/plugins/indent-blackline.lua
+++ lua/plugins/indent-blackline.lua
@@ -1,9 +1,4 @@
-require 'indent_blankline'.setup {
-    show_current_context = true,
-    show_current_context_start = true,
-    show_end_of_line = true,
-    space_char_blankline = " ",
-}
+require 'ibl'.setup {}
 
 vim.opt.list = true
 --vim.opt.listchars:append("space:.")
